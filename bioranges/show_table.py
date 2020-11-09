f97bfb0620fb1bb3ae77585e4b77b84141cdd7fa
Fedor P. Goncharov <gfederix@gmail.com>
2020-11-09 18:54:45 +0700

    single row table is work

--- bioranges/show_table.py
+++ bioranges/show_table.py
@@ -1,9 +1,25 @@
+from functools import partial
+
 def show_table(x=None):
     if x is None:
         return ""
     r1 = x[0]
     r1_name, r1_data = r1
-    return "\n".join(row_presenter(r1_name, type(r1_data).__name__, r1_data))
+    r1_width = max(len(r1_name), 10)
+    return "\n".join(row_presenter(r1_name, type(r1_data).__name__, r1_data, width=r1_width))
 
-def row_presenter(name, type, data):
-    return [str(name), "<{}>".format(type)] + list(map(str, data))
+
+def cell_presenter(x, width):
+    return "{{:>{}}}".format(width).format(x)
+
+
+def type_cell_presenter(x, width):
+    x = "<{}>".format(x)
+    return cell_presenter(x, width)
+
+
+def row_presenter(name, type, data, width):
+    return ([
+        cell_presenter(name, width),
+        type_cell_presenter(type, width)] +
+        list(map(partial(cell_presenter, width=width), data)))
--- tests/test_display.py
+++ tests/test_display.py
@@ -1,18 +1,42 @@
 import pytest
-from bioranges import show_table
-from bioranges.show_table import row_presenter
 import numpy as np
+
+# local
+from bioranges import show_table
+from bioranges.show_table import row_presenter, cell_presenter, type_cell_presenter
+
 
 def test_sum():
     assert 1 + 1 == 2
 
+
 def test_epty_show_table():
     assert show_table() == ""
 
+
 def test_single_column_show_table():
     out_str = show_table([("first_name", [1, 2, 3])])
-    assert out_str == "first_name\n<list>\n1\n2\n3"
+    assert out_str == (
+        "first_name\n"
+        "    <list>\n"
+        "         1\n"
+        "         2\n"
+        "         3")
+
 
 def test_row_presenter():
-    out_array = row_presenter(name="first_name", type="array", data=[1, 2, 3])
-    assert out_array == ["first_name", "<array>", "1", "2", "3"]
+    out_array = row_presenter(name="first_name", type="array", data=[1, 2, 3], width=12)
+    assert out_array == [
+        "  first_name",
+        "     <array>",
+        "           1",
+        "           2",
+        "           3"]
+
+
+def test_cell_presenter():
+    assert cell_presenter(100, 5) == "  100"
+
+
+def test_cell_presenter():
+    assert type_cell_presenter("some_type", 15) == "    <some_type>"
