dfee0e7747e9023a36c277304f984c9b51ac357c
Fedor P. Goncharov <gfederix@gmail.com>
2020-12-02 15:07:37 +0700

    todo io abstract classes

--- bioranges/io.py
+++ bioranges/io.py
@@ -1,7 +1,22 @@
+from abc import ABC
+from abc import abstractmethod
 from collections import namedtuple
 
-
 TrackLine = namedtuple('TrackLine', ['name'])
+
+
+# TODO: here probably need create adaptor class for init: Range(BadFile(file)),
+# Range(Dict({})), Range(WigFile())
+class ReadFile(ABC):
+    @abstractmethod
+    def trackline() -> TrackLine:
+        pass
+
+
+class RangeAdapter(ABC):        # TODO: to acess SQL and etc
+    @abstractmethod
+    def trackline() -> TrackLine:
+        pass
 
 
 class FackedRange:
