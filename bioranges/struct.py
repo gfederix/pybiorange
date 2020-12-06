1ac38f18e9a53a3db99fdcf2a5ed8c62866bc1b8
Fedor P. Goncharov <gfederix@gmail.com>
2020-12-07 04:47:54 +0700

    use first four bit to save & operate dna char

--- bioranges/struct.py
+++ bioranges/struct.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class DNAString:
     def __init__(self, x):
         pass
@@ -10,18 +13,31 @@ class DNAString:
         return 0b00000001
 
 
-COMP_PAIRS = [
-    ('A', 'T'),
-    ('G', 'C'),
-    ('M', 'K'),
-]
+DNA_MAP = np.array([            # ..CTGA
+    '-',                        # 0b0000 -
+    'A',                        # 0b0001 A
+    'G',                        # 0b0010 G
+    'R',                        # 0b0011 GA == puRine
+    'T',                        # 0b0100 T
+    'W',                        # 0b0101 TA == Weak
+    'K',                        # 0b0110 TG == Keto
+    'D',                        # 0b0111 TGA == Not C -> D
+    'C',                        # 0b1000 C
+    'M',                        # 0b1001 CA == aMino
+    'S',                        # 0b1010 CG == Strong
+    'V',                        # 0b1011 CGA == Not T -> V
+    'Y',                        # 0b1100 TC == pYrimidine
+    'H',                        # 0b1101 TCA == Not G -> H
+    'B',                        # 0b1110 CTG == Not A -> B
+    'N',                        # 0b1111 TCGA = aNy
+])
 
-MAX_BIT = 0
-DNA_BIT_MAP = {}
-for x, y in COMP_PAIRS:
-    DNA_BIT_MAP[x] = MAX_BIT
-    DNA_BIT_MAP[y] = ~MAX_BIT
-    MAX_BIT += 1
+DNA_BIT_MAP = {i: x for x, i in enumerate(DNA_MAP)}
+
+
+def complement(x):
+    return x >> 2 | (x << 2 & 0b1100)
+
 
 IUPAC_CODE_MAP = {
     'A': 'A',
--- tests/dnastring_test.py
+++ tests/dnastring_test.py
@@ -1,5 +1,6 @@
 import pytest
 
+from bioranges.struct import complement
 from bioranges.struct import DNA_BIT_MAP
 from bioranges.struct import DNAString
 
@@ -21,6 +22,13 @@ def test_binarization():
 def test_complementaryity_in_binarization():
     # DNAString.tobit("A") == 0b1
     # DNAString.tobit("T") == 0b1
-    assert DNA_BIT_MAP['A'] == ~DNA_BIT_MAP['T']
-    assert DNA_BIT_MAP['G'] == ~DNA_BIT_MAP['C']
+    assert DNA_BIT_MAP['A'] == complement(DNA_BIT_MAP['T'])
+    assert DNA_BIT_MAP['G'] == complement(DNA_BIT_MAP['C'])
     assert DNA_BIT_MAP['A'] != DNA_BIT_MAP['G']
+
+    assert DNA_BIT_MAP['W'] == complement(DNA_BIT_MAP['W'])
+    assert DNA_BIT_MAP['S'] == complement(DNA_BIT_MAP['S'])
+    assert DNA_BIT_MAP['M'] == complement(DNA_BIT_MAP['K'])
+    assert DNA_BIT_MAP['K'] == complement(DNA_BIT_MAP['M'])
+    assert DNA_BIT_MAP['N'] == complement(DNA_BIT_MAP['N'])
+    assert DNA_BIT_MAP['V'] == complement(DNA_BIT_MAP['B'])
